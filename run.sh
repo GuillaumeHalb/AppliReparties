c16b60cdc8ea728d789ffc1229247bf8024275ea
Benjamin Lebit <benjamin.lebit@ensimag.grenoble-inp.fr>
2016-11-28 18:51:35 +0100

    run

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-xterm -hold -e "rmiregistry" &
-##xterm --hold -e java -jar Server/target/*.jar rmi
+xterm --hold -e java -jar Server/target/*.jar rmi &
 xterm -hold -e java -jar Server/target/*.jar SG 1 &
 xterm -hold -e java -jar Server/target/*.jar BNP 2 &
 xterm -hold -e java -jar Server/target/*.jar Natixis 3 &
