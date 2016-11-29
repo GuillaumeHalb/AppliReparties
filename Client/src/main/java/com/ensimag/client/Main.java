104bc6fc5c76ecb369d5c9ffa5baf7121e114cc1
GuillaumeHalb <guillaume.halb@ensimag.grenoble-inp.fr>
2016-11-29 16:28:04 +0100

    corriger erreur compilation

--- Client/src/main/java/com/ensimag/client/Main.java
+++ Client/src/main/java/com/ensimag/client/Main.java
@@ -10,33 +10,27 @@ import com.ensimag.server.impl.BankNode;
 import com.ensimag.services.bank.IBankNode;
 
 public class Main {
-    public static void main(String[] args) throws RemoteException, NotBoundException, MalformedURLException
-    {
-    	 ArrayList<BankNode> bankNodeList = new ArrayList<BankNode>();
-         BankNode SG = (BankNode) Naming.lookup("rmi://localhost/SG");
-         bankNodeList.add(SG);
-         BankNode BNP = (BankNode) Naming.lookup("rmi://localhost/BNP");
-         bankNodeList.add(BNP);
-         BankNode Natixis = (BankNode) Naming.lookup("rmi://localhost/Natixis");
-         bankNodeList.add(Natixis);
-         
-         
-        
-        try {            
-	            for(BankNode bank : bankNodeList)
-	            {
-	           	 System.out.println("Name : " + bank.getBank());
-	           	 System.out.println("Id : " + bank.getId());
-	           	 for (BankNode neighboor : bank.getNeighboors())
-	           	 	{
-	           		 System.out.println(neighboor.getBank().getBankName());
-	           	 	}
-	            }
-	    //            System.out.println(op.div(33,0));        
-        } catch (Exception e)
-        {
-            System.err.println(e);
-        }
+	public static void main(String[] args) throws RemoteException, NotBoundException, MalformedURLException {
+		ArrayList<BankNode> bankNodeList = new ArrayList<BankNode>();
+		BankNode SG = (BankNode) Naming.lookup("rmi://localhost/SG");
+		bankNodeList.add(SG);
+		BankNode BNP = (BankNode) Naming.lookup("rmi://localhost/BNP");
+		bankNodeList.add(BNP);
+		BankNode Natixis = (BankNode) Naming.lookup("rmi://localhost/Natixis");
+		bankNodeList.add(Natixis);
 
-    }
+		try {
+			for (BankNode bank : bankNodeList) {
+				System.out.println("Name : " + bank.getBank());
+				System.out.println("Id : " + bank.getId());
+				for (IBankNode neighboor : bank.getNeighboors()) {
+					System.out.println(((BankNode) neighboor).getBank().getBankName());
+				}
+			}
+			// System.out.println(op.div(33,0));
+		} catch (Exception e) {
+			System.err.println(e);
+		}
+
+	}
 }
