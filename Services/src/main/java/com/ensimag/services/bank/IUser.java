72aa884c8f041c2db445a02833757ae17236354a
GuillaumeHalb <guillaume.halb@ensimag.grenoble-inp.fr>
2016-12-06 13:54:57 +0100

    adding Remote to IUser

--- Services/src/main/java/com/ensimag/services/bank/IUser.java
+++ Services/src/main/java/com/ensimag/services/bank/IUser.java
@@ -1,22 +1,24 @@
 package com.ensimag.services.bank;
 
 import java.io.Serializable;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
-public interface IUser extends Serializable {
+public interface IUser extends Serializable, Remote {
 
 	/**
 	 * @return the name of the user
 	 */
-	String getName();
+	String getName() throws RemoteException;
 
 	/**
 	 * @return the first name
 	 */
-	String getFirstName();
+	String getFirstName() throws RemoteException;
 
 	/**
 	 * @return the age
 	 */
-	String getAge();
+	String getAge() throws RemoteException;
 
 }
