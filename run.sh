dd596eec38fa6d1d7054bbc20784c6a5fe3d7de0
GuillaumeHalb <guillaume.halb@ensimag.grenoble-inp.fr>
2016-12-08 16:39:42 +0100

    correction du graphe

--- run.sh
+++ run.sh
@@ -10,11 +10,11 @@ gnome-terminal --execute java -jar Server/target/*.jar Barclays 6 start
 gnome-terminal --execute java -jar Server/target/*.jar BNP 7 start
 gnome-terminal --execute java -jar Server/target/*.jar Deutsche_Bank 8 start
  #adding neighboor
-gnome-terminal --execute java -jar Server/target/*.jar Goldman_Sachs JP_Morgan,Merill_Lynch add
+gnome-terminal --execute java -jar Server/target/*.jar Goldman_Sachs JP_Morgan,Merill_Lynch,HSBC add
 gnome-terminal --execute java -jar Server/target/*.jar  JP_Morgan Goldman_Sachs,Citi add
-gnome-terminal --execute java -jar Server/target/*.jar Citi Merill_Lynch,Barclays,BNP add
+gnome-terminal --execute java -jar Server/target/*.jar Citi Merill_Lynch,Barclays,BNP,JP_Morgan add
 gnome-terminal --execute java -jar Server/target/*.jar Merill_Lynch Goldman_Sachs,Citi,HSBC add
-gnome-terminal --execute java -jar Server/target/*.jar HSBC Merill_Lynch,Barclays add
+gnome-terminal --execute java -jar Server/target/*.jar HSBC Merill_Lynch,Barclays,Goldman_Sachs add
 gnome-terminal --execute java -jar Server/target/*.jar Barclays Citi,HSBC,BNP add
 gnome-terminal --execute java -jar Server/target/*.jar BNP Barclays,Citi,Deutsche_Bank add
 gnome-terminal --execute java -jar Server/target/*.jar Deutsche_Bank BNP add
