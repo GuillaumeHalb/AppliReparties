43827ffa78e0509e08860585a5c64fcff0643185
Benjamin Lebit <benjamin.lebit@ensimag.grenoble-inp.fr>
2016-12-13 12:35:55 +0100

    get account

---
+++ Server/src/main/java/com/ensimag/server/impl/GetAccount.java
@@ -0,0 +1,26 @@
+package com.ensimag.server.impl;
+
+import java.io.Serializable;
+
+import com.ensimag.services.bank.IBankAction;
+import com.ensimag.services.bank.IBankNode;
+
+public class GetAccount implements IBankAction {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8717717905900651015L;
+	
+	private long accountNumber;
+	
+	public GetAccount(long accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	@Override
+	public Serializable execute(IBankNode node) throws Exception {
+		return node.getAccount(accountNumber);
+	}
+
+}
